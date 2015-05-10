19153e02e22f83d912f1a5798c568b9e7ec07337
Alex Calfo <alex@swarmingtech.com>
2015-05-10 18:20:22 -0500

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -2,19 +2,21 @@ app = angular.module('app', [
   'ui.router'
   ])
 
-  .config(function($stateProvider, $urlRouterProvider) {
+.config(
+  function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("/");
     
     $stateProvider
       .state('home', {
         resolve:{
-          init: function(contact){
+          init: function( contact ) {
             return contact.init();
           }
-       },
-        url: "/",
-        templateUrl: "views/home.html",
+        },
+        url: '/',
+        templateUrl: 'views/home.html',
         controller: 'mainController'
-      });
+      }
+    );
   }
 );
